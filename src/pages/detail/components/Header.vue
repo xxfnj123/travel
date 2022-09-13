a324bfad88c637fa020e21d4a476e026f7f588f0
xxfnj123 <2623734202@qq.com>
2022-09-13 21:20:44 +0800

    list finish

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -61,22 +61,23 @@ export default {
       color: #fff
       font-size: .4rem
   .header-fixed
-        position: fixed
-        top: 0
-        left: 0
-        right: 0
-        height: $headerHeight
-        line-height: $headerHeight
-        text-align: center
-        color: #fff
-        background: $bgColor
-        font-size: .32rem
-        .header-fixed-back
-            position: absolute
-            top: 0
-            left: 0
-            width: .64rem
-            text-align: center
-            font-size: .4rem
-            color: #fff
+    z-index: 2
+    position: fixed
+    top: 0
+    left: 0
+    right: 0
+    height: $headerHeight
+    line-height: $headerHeight
+    text-align: center
+    color: #fff
+    background: $bgColor
+    font-size: .32rem
+    .header-fixed-back
+      position: absolute
+      top: 0
+      left: 0
+      width: .64rem
+      text-align: center
+      font-size: .4rem
+      color: #fff
 </style>
